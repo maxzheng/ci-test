35f5660053f5a2340ae12ec0466ca1121d44a67e
Max Zheng <maxzheng.os@gmail.com>
2018-12-19 22:33:44 +0000

    Add more tests

--- test/test_jenkins_test.py
+++ test/test_jenkins_test.py
@@ -1,6 +1,6 @@
-from time import sleep
-
-
 def test_jenkins_test():
     """ Test is code's best friend. ^_^ """
-    sleep(86400)
+
+
+def test_jenkins_test_2():
+    """ Test is code's best friend. ^_^ """
